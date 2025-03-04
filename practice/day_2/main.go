4179b9d47156df2e81da8833faa83594f5874673
mahabub ahmed <mahabub.ahmed.eee@gmail.com>
2025-03-04 22:40:23 +0100

    2nd day half

---
+++ practice/day_2/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main(){
+	arr := [5]int{1,2,3,4,5}
+	slice := arr[:3]
+	// fmt.Println(slice, len(slice), cap(slice))
+	// slice2 := []int{1,2,3}
+	// fmt.Println(slice2, len(slice2), cap(slice2))
+	// slice3 := make([]int, 5, 9)
+	// fmt.Println(slice3, len(slice3), cap(slice3))
+	slice = append(slice, 6, 7,8)
+	arr[0] = 10
+	slice = append(arr[:2], arr[3:]...)
+	fmt.Println(slice, len(slice), cap(slice))
+	// fmt.Println(slice2, len(slice2), cap(slice2))
+}
